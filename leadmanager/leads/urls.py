df0589eafc0e559af9504e395f3c1b68b48377c6
Denis <ddujota@internationalsafety.com>
2019-08-18 17:48:32 -0400

    added comments

--- leadmanager/leads/urls.py
+++ leadmanager/leads/urls.py
@@ -1,7 +1,9 @@
 from rest_framework import routers
 from .api import LeadViewSet
 
+# NameSpace our API url endpoints
 router = routers.DefaultRouter()
 router.register('api/leads', LeadViewSet, 'leads')
 
+# Only use the registered url endpoints
 urlpatterns = router.urls
