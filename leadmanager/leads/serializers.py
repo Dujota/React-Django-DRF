a177b847a365bd5649945deccb2ac584499f3fe2
Denis <ddujota@internationalsafety.com>
2019-08-18 17:17:36 -0400

    serialzier created for the leads model

---
+++ leadmanager/leads/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from leads.models import Lead
+
+# Lead Serializer - rest_framework docs -> serializers
+
+
+class LeadSerializer(searializers.ModelSerializer):
+    class Meta:
+        model = Lead
+        fields = '__all__'
