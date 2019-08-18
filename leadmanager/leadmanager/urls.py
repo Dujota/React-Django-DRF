07d1383c36a89e192905852696afefdfd914fa06
Denis <ddujota@internationalsafety.com>
2019-08-18 19:12:24 -0400

    fixed script and trailing comma on urls folder and then ran dev script

--- leadmanager/leadmanager/urls.py
+++ leadmanager/leadmanager/urls.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 """
-Load the front end first and have those urls exposed then load up the api urls
+Load the front end first
+Then load up the api urls
 
 """
 urlpatterns = [
-    path('', include('frontend.urls'))
+    path('', include('frontend.urls')),
     path('', include('leads.urls')),
 ]
