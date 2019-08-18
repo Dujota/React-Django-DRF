e924506da7b0e79a9c1ccb04917dabe8c77bc277
Denis <ddujota@internationalsafety.com>
2019-08-18 19:06:13 -0400

    Added the frontend urls to the project root

--- leadmanager/leadmanager/urls.py
+++ leadmanager/leadmanager/urls.py
@@ -2,6 +2,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+"""
+Load the front end first and have those urls exposed then load up the api urls
+
+"""
 urlpatterns = [
+    path('', include('frontend.urls'))
     path('', include('leads.urls')),
 ]
