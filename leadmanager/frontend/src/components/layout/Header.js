c56ac309b43247b0b70181db3a2e42d987320dc1
Denis <ddujota@internationalsafety.com>
2019-08-21 12:18:13 -0400

    switched to bootstrap and then added in the app component, added the main.js to gitignore

--- leadmanager/frontend/src/components/layout/Header.js
+++ leadmanager/frontend/src/components/layout/Header.js
@@ -1,22 +1,69 @@
-import React, { Component, Fragment } from 'react';
+import React, { PureComponent } from 'react';
 
-export default class Header extends Component {
+export default class Header extends PureComponent {
   render() {
     return (
-      <Fragment>
-        <div className="ui secondary pointing menu">
-          <a className="item">Lead Manager</a>
-          <a className="item">Home</a>
-          <a className="item">Messages</a>
-          <a className="item ">Friends</a>
-          <div className="right menu">
-            <a className="ui item">Logout</a>
-          </div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="#">
+                  Action
+                </a>
+                <a className="dropdown-item" href="#">
+                  Another action
+                </a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </div>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">
+                Disabled
+              </a>
+            </li>
+          </ul>
         </div>
-        <div className="ui segment">
-          <p></p>
-        </div>
-      </Fragment>
+      </nav>
     );
   }
 }
